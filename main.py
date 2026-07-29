4c6a4528894b037ca9530037afef6a90047b6fb7
Oluwaseun Adegbesan <68032624+oluwaseunbaba@users.noreply.github.com>
2026-07-29 17:06:30 +0100

    Integrate weather API and remove birthday email logic
    Removed birthday email functionality and added weather API integration.

--- main.py
+++ main.py
@@ -4,35 +4,55 @@
 # 3. Update the SMTP ADDRESS to match your email provider.
 # 4. Update birthdays.csv to contain today's month and day.
 # See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
 import os
+import requests
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+OWN_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = os.environ.get("OWN_API_KEY")
+account_sid = os.environ.get("ACCUNT_SID")
+auth_token =  os.environ.get("AUTH_TOKEN")
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+parameters = {
+        "lat":6.451140,
+        "lon":3.388400,
+        "appid":api_key,
+        "cnt":4
+            }
+
+response = requests.get(OWN_Endpoint,params=parameters)
+response.raise_for_status()
+print(response.status_code)
+data = response.json()
+
+hour_3 = data["list"][0]["weather"][0]["id"]
+hour_6 = data["list"][1]["weather"][0]["id"]
+hour_9 = data["list"][2]["weather"][0]["id"]
+hour_12 = data["list"][3]["weather"][0]["id"]
+
+will_rain = False
+
+# if hour_3 < 700 or hour_6 < 700 or hour_9 < 700 or hour_12 < 700 :
+#     print("bring an umbrella")
+
+for hour in data["list"]:
+    condition_code = hour["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    proxy_client = TwilioHttpClient(proxy={'http': os.environ['http_proxy'], 'https': os.environ['https_proxy']})
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    message = client.messages.create(
+        body="Its going to rain today don't forget to bring an Umbrella ☂️",
+        from_="whatsapp:+14155238886",
+        to="whatsapp:+2348032009394",
+    )
+
+    # noinspection string-conversion-without-dunder-method
+    print(message.status)
+
+
